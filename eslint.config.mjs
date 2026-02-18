5a9e915ca5cf1ffc9886913ea61548a863d62160
Luiz Castro <luizcastro2122@gmail.com>
2026-02-18 00:28:54 -0300

    config: docker-compose database

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import tseslint from "typescript-eslint";
 import prettierPlugin from "eslint-plugin-prettier";
 
 export default tseslint.config({
-  ignores: ["dist/**"],
+  ignores: ["dist/**", "node_modules/**"],
   files: ["**/*.ts"],
   extends: [...tseslint.configs.recommended],
   plugins: {
@@ -24,6 +24,9 @@ export default tseslint.config({
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-empty-function": ["error", { "allow": ["constructors"] }],
+    "@typescript-eslint/no-empty-function": [
+      "error",
+      { allow: ["constructors"] },
+    ],
   },
 });
