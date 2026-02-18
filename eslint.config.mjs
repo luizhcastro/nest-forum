5939867ca7c03324451b256dc6e459bbf29142df
Luiz Castro <luizcastro2122@gmail.com>
2026-02-18 00:01:35 -0300

    config: eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import tseslint from "typescript-eslint";
+import prettierPlugin from "eslint-plugin-prettier";
+
+export default tseslint.config({
+  files: ["**/*.ts"],
+  extends: [...tseslint.configs.recommended],
+  plugins: {
+    prettier: prettierPlugin,
+  },
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        printWidth: 80,
+        tabWidth: 2,
+        singleQuote: true,
+        trailingComma: "all",
+        arrowParens: "always",
+        semi: false,
+      },
+    ],
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+  },
+});
